2ff655800080eebf1557a6acb97d5c3d09da8652
skshin <skshin@gmail.com>
2018-04-18 17:59:36 +0900

    admin.php: add config session value to admin

--- admin.php
+++ admin.php
@@ -1,23 +1,34 @@
 <?php
-include_once 'dbConnection.php';
-$ref=@$_GET['q'];
-$email = $_POST['uname'];
-$password = $_POST['password'];
+	include_once 'dbConnection.php';
+	$ref=@$_GET['q'];
+	$email = $_POST['uname'];
+	$password = $_POST['password'];
 
-$email = stripslashes($email);
-$email = addslashes($email);
-$password = stripslashes($password); 
-$password = addslashes($password);
-$result = mysqli_query($con,"SELECT email FROM admin WHERE email = '$email' and password = '$password'") or die('Error');
-$count=mysqli_num_rows($result);
-if($count==1){
-session_start();
-if(isset($_SESSION['email'])){
-session_unset();}
-$_SESSION["name"] = 'Admin';
-$_SESSION["key"] ='sunny7785068889';
-$_SESSION["email"] = $email;
-header("location:dash.php?q=0");
-}
-else header("location:$ref?w=Warning : Access denied");
+	$email = stripslashes($email);
+	$email = addslashes($email);
+	$password = stripslashes($password); 
+	$password = addslashes($password);
+	$result = mysqli_query($con,"SELECT email FROM admin WHERE email = '$email' and password = '$password'") or die('Error');
+	$count=mysqli_num_rows($result);
+	if($count==1)
+	{
+		session_start();
+		if(isset($_SESSION['email'])){
+		session_unset();}
+		$_SESSION["name"] = 'Admin';
+		$_SESSION["key"] ='sunny7785068889';
+		$_SESSION["email"] = $email;
+		
+		$config = array();
+		$q = mysqli_query($con, "SELECT * FROM `config`");
+		while ($row = mysqli_fetch_array($q))
+		{
+			$config[$row['ckey']] = $row['cvalue'];
+		}
+		$_SESSION["config"] = $config;
+
+		header("location:dash.php?q=0");
+	}
+	else 
+		header("location:$ref?w=Warning : Access denied");
 ?>
